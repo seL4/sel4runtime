f874576e115eb2b648acbb4b708e62edc93f749d
Curtis Millar <curtis.millar@data61.csiro.au>
2018-09-03 16:13:52 +1000

    Add dummy runtime.

---
+++ src/start.c
@@ -0,0 +1,16 @@
+#include <sel4/sel4.h>
+#include "start.h"
+
+void __sel4rt_start_main(
+	int (*main)(),
+	unsigned long argc,
+	char **argv,
+	char **envp,
+	auxv_t auxv[]
+) {
+	// Simple start to begin with.
+	main(argc, argv, envp);
+	while (1) {
+		seL4_Yield();
+	}
+}
