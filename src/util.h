83421073e00c713062c6f6101d96f7ac699269ba
Curtis Millar <curtis.millar@data61.csiro.au>
2018-09-24 14:41:21 +1000

    Licenses.

--- src/util.h
+++ src/util.h
@@ -1,3 +1,14 @@
+/*
+ * Copyright 2018, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
 #define ALIGN_UP(x, n) (((x) + (n) - 1) & ~((n) - 1))
 #define ALIGN_DOWN(x, n) ((x) & ~((n) - 1))
 
