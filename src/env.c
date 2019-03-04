3afdd8beacd1faef1126af26d1ead8b06a4db05d
Anna Lyons <Anna.Lyons@data61.csiro.au>
2019-03-04 17:21:14 +1100

    Set process name if tcb and ipc buffer are set
    Otherwise it crashes.

--- src/env.c
+++ src/env.c
@@ -156,8 +156,9 @@ uintptr_t sel4runtime_move_initial_tls(void *tls_memory) {
     env.initial_thread_tls_base = tls_base;
 
 #if defined(CONFIG_DEBUG_BUILD)
-    if (env.initial_thread_tcb) {
-        // The thread can only be named after the TLS is initialised.
+    if (env.initial_thread_tcb && env.initial_thread_ipc_buffer) {
+        // The thread can only be named after the TLS is initialised
+        // and if an IPC buffer is present.
         seL4_DebugNameThread(env.initial_thread_tcb, env.process_name);
     }
 #endif
