ed74e62d06ab9de312ec4f6ee0d4c118704c908d
Kent McLeod <kent@kry10.com>
2021-09-15 14:05:40 +1000

    Remove ARMv6 support
    ARMv6 support is being removed from seL4.
    
    Signed-off-by: Kent McLeod <kent@kry10.com>

--- include/sel4_arch/aarch32/sel4runtime/thread_arch.h
+++ include/sel4_arch/aarch32/sel4runtime/thread_arch.h
@@ -7,12 +7,7 @@
 #include <sel4/arch/constants.h>
 #include <sel4runtime/stdint.h>
 
-#if ((\
-            defined(__ARM_ARCH_6K__) || \
-            defined(__ARM_ARCH_6ZK__) \
-        ) && !defined(__thumb__) \
-    ) || \
-    defined(__ARM_ARCH_7A__) || \
+#if defined(__ARM_ARCH_7A__) || \
     defined(__ARM_ARCH_7R__) || \
     (defined(__ARM_ARCH) && __ARM_ARCH >= 7)
 
@@ -43,39 +38,6 @@ static inline void sel4runtime_set_tls_base(sel4runtime_uintptr_t tls_base)
     sel4runtime_write_tpidr_el0(tls_base);
 }
 
-#elif defined(CONFIG_KERNEL_GLOBALS_FRAME)
-
-/*
- * In the case of early versions of ARMv6, there are no hardware
- * registers provided for thread-local identifiers. seL4 resolves this
- * by placing the IPC buffer address and thread pointer in a
- * `GlobalsFrame` mapped at the same address in all virtual address
- * spaces. The IPC buffer and thread pointer occupy the first two words
- * in this frame respectively.
- */
-static inline sel4runtime_uintptr_t sel4runtime_read_tpidr_el0(void)
-{
-    void **globals_frame = (void **)seL4_GlobalsFrame;
-    return (sel4runtime_uintptr_t)globals_frame[0];
-}
-
-static inline sel4runtime_uintptr_t sel4runtime_read_tpidrro_el0(void)
-{
-    void **globals_frame = (void **)seL4_GlobalsFrame;
-    return (sel4runtime_uintptr_t)globals_frame[1];
-}
-
-#ifdef CONFIG_SET_TLS_BASE_SELF
-/*
- * Set the value of the TLS base for the current thread.
- */
-static inline void sel4runtime_set_tls_base(sel4runtime_uintptr_t tls_base)
-{
-    seL4_SetTLSBase(tls_base);
-}
-#else
-#error "No way to set TLS base provided."
-#endif
 #else
 #error "No TLS mechanism provided."
 #endif /* CONFIG_SET_TLS_BASE_SELF */
